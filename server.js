9af4a899c5f7f33ddd01547c76bab2bd8a9fbe87
Paola Guarasci <paolaguarasci@gmail.com>
2016-09-06 16:52:38 +0200

    finisch!

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 var express = require('express')
 var app = express()
-// var url = require('url-parse')
-var moment = require('moment')
-// var jquery = require('jquery')
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 3000
 
-app.get('/:time', function (req, res) {
-  var path = req.params.time
-  var data = null
+app.get('/', function (req, res) {
+  var useragent = req.headers
+  var ip = req.headers.host
+  var lang = useragent['accept-language'].match(/[a-zA-Z]{2}-[a-zA-Z]{1}[^;]/)
+  var system = useragent['user-agent'].match(/\(([^()]+)\)/)
   var result = {}
-  if (parseInt(path, 10)) {
-    data = moment(parseInt(path, 10), 'X')
+  // console.log(system[0].substring(1,system[0].length-1))
+  if (!useragent) {
+    result.ip = null
+    result.language = null
+    result.system = null
   } else {
-    data = moment(path)
-  }
-  if (!data.isValid()) {
-    result.unix = null
-    result.natural = null
-  } else {
-    result.unix = data.format('X')
-    result.natural = data.format('MMMM DD, YYYY')
+    result.ipaddress = ip
+    result.language = lang[0]
+    result.software = system[0].substring(1, system[0].length - 1)
   }
   res.send(JSON.stringify(result))
 })
+
 app.use(express.static('./'))
 app.listen(port)
