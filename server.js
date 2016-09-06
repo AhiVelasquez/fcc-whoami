8eeafc681c1b67e698921f11c2bb9e6d2788e4f1
Paola Guarasci <paolaguarasci@gmail.com>
2016-09-06 17:26:33 +0200

    try error fix (no ip address)

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express')
 var app = express()
+var getIP = require('ipware')().get_ip
 var port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   var useragent = req.headers
-  var ip = req.connection.remoteAddress
+  var ip = req.connection.remoteAddress.match(/[^::ffff:](\d+.+)/)
   var lang = useragent['accept-language'].match(/[a-zA-Z]{2}-[a-zA-Z]{1}[^;]/)
   var system = useragent['user-agent'].match(/\(([^()]+)\)/)
   var result = {}
-  // console.log(req.connection.remoteAddress)
+  console.log(ip[0])
   if (!useragent) {
     result.ip = null
     result.language = null
     result.system = null
   } else {
-    result.ipaddress = ip
+    result.ipaddress = ip[0]
     result.language = lang[0]
     result.software = system[0].substring(1, system[0].length - 1)
   }
