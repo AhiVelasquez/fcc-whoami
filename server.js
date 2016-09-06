6dd9e0ac4bfc2b82c38e204504d5e9dcb5aeae7f
Paola Guarasci <paolaguarasci@gmail.com>
2016-09-06 17:36:09 +0200

    try error fix (no ip address)

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ var port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   var useragent = req.headers
-  var ip = req['X-Forwarded-For'].match(/[^::ffff:](\d+.+)/)
+  var heroku = req.headers['X-Forwarded-For']
+  // .match(/[^::ffff:](\d+.+)/)
   var lang = useragent['accept-language'].match(/[a-zA-Z]{2}-[a-zA-Z]{1}[^;]/)
   var system = useragent['user-agent'].match(/\(([^()]+)\)/)
   var result = {}
-  console.log(ip[0])
-  if (!useragent) {
-    result.ip = null
-    result.language = null
-    result.system = null
+  var ip = ''
+  if (heroku) {
+    var list = ipAddr.split(",")
+    ip = list[list.length-1]
   } else {
+    ip = req.connection.remoteAddress.match(/[^::ffff:](\d+.+)/)
+  }
     result.ipaddress = ip[0]
     result.language = lang[0]
     result.software = system[0].substring(1, system[0].length - 1)
-  }
   res.send(JSON.stringify(result))
 })
 
